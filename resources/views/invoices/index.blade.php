d14c2edca5df3b45581994cfac6432611efa84f5
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-06-16 22:25:23 +0200

    unhappy werkt nu ook

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -116,7 +116,24 @@
                                 </td>
                                 <td class="px-6 py-4 text-sm">
                                     <a href="{{ route('invoices.show', $invoice->id) }}" class="text-blue-500 hover:underline">Bekijken</a>
-                                    <a href="{{ route('invoices.edit', $invoice->id) }}" class="text-yellow-500 hover:underline ml-4">Bewerken</a>
+                                    @if(strtolower($invoice->invoice_status) === 'paid')
+                                        <span
+                                            class="text-yellow-300 cursor-not-allowed ml-4 relative group"
+                                            style="opacity: 0.6;"
+                                            title=""
+                                        >
+                                            Bewerken
+                                            <span class="absolute left-1/2 -translate-x-1/2 bottom-full mb-2 w-48 bg-gray-800 text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity pointer-events-auto z-10">
+                                                Betaalde facturen kunnen niet meer bewerkt worden.
+                                            </span>
+                                        </span>
+                                    @else
+                                        <a href="{{ route('invoices.edit', $invoice->id) }}"
+                                           class="text-yellow-500 hover:underline ml-4"
+                                           title="Factuur bewerken">
+                                            Bewerken
+                                        </a>
+                                    @endif
                                     <form action="{{ route('invoices.destroy', $invoice->id) }}" method="POST" class="inline">
                                         @csrf
                                         @method('DELETE')
